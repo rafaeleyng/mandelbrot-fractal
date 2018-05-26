8765225210e48614cad4c173e6438e0733955ebf
Rafael Eyng <rafaeleyng@gmail.com>
2018-05-25 23:46:02 -0300

    init

---
+++ mandelbrot-fractal/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  mandelbrot-fractal
+//
+//  Created by Rafael Eyng on 5/25/18.
+//  Copyright © 2018 Rafael Eyng. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
